811a8dfea5407f283189e6294ec13f8b74f69c52
savir-nosaj <janasty30@gmail.com>
2025-07-16 13:39:47 -0700

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
@@ -8,8 +9,10 @@ export default defineConfig([
   globalIgnores(['dist']),
   {
     files: ['**/*.{js,jsx}'],
+    settings: { react: { version: 'detect' } },
     extends: [
       js.configs.recommended,
+      react,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
     ],
@@ -23,7 +26,12 @@ export default defineConfig([
       },
     },
     rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'no-unused-vars': 'warn', //this changes the error to a warning
+      // 'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      ...js.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
+      'react/prop-types': 'off', //this suppresses warnings about not using prop-types
     },
   },
 ])
